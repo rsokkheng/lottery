3969de77115230b61323286a9a59644a6d4f5293
rsokkheng <sokkhengry168@gmail.com>
2025-02-06 23:52:58 +0700

    fix style menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,80 +1,34 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav x-data="{ open: false }" class="bg-white border-b border-gray-100" style="background-color:rgb(37 99 235)">
     <!-- Primary Navigation Menu -->
     <div class="max-w-12xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-                <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-{{--                    <a href="{{ route('dashboard') }}">--}}
-                    <a>
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-                    </a>
+                    <a style="color:yellow;font-weight: 800;font-size: 26px;">HAPPY SPORT 888</a>
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-20 sm:flex">
-{{--                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">--}}
-                    <x-nav-link>
-                        {{ __('Bets') }}
-                    </x-nav-link>
+                <div class="hidden sm:flex sm:ms-10 space-x-8">
+                    <x-nav-link style="font-size: 16px;font-weight: 800; color:white">{{ __('Bet') }}</x-nav-link>
+                    <x-nav-link style="font-size: 16px;font-weight: 800; color:white">{{ __('Receipt List') }}</x-nav-link>
+                    <x-nav-link style="font-size: 16px;font-weight: 800; color:white">{{ __('Bet List') }}</x-nav-link>
+                    <x-nav-link style="font-size: 16px;font-weight: 800; color:white">{{ __('Bet Number') }}</x-nav-link>
+                    <x-nav-link style="font-size: 16px;font-weight: 800; color:white">{{ __('Total Bet Number') }}</x-nav-link>
+                    <x-nav-link style="font-size: 16px;font-weight: 800; color:white">{{ __('Win Report') }}</x-nav-link>
+                    <x-nav-link style="font-size: 16px;font-weight: 800; color:white">{{ __('Daily Report') }}</x-nav-link>
+                    <x-nav-link style="font-size: 16px;font-weight: 800; color:white">{{ __('Summary Report') }}</x-nav-link>
+                    <x-nav-link style="font-size: 16px;font-weight: 800; color:white">{{ __('Results') }}</x-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-20 sm:flex">
-{{--                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">--}}
-                    <x-nav-link>
-                        {{ __('Receipt List') }}
-                    </x-nav-link>
-                </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-20 sm:flex">
-{{--                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">--}}
-                    <x-nav-link>
-                        {{ __('Bet List') }}
-                    </x-nav-link>
-                </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-20 sm:flex">
-{{--                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">--}}
-                    <x-nav-link>
-                        {{ __('Bet Number') }}
-                    </x-nav-link>
-                </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-20 sm:flex">
-{{--                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">--}}
-                    <x-nav-link>
-                        {{ __('Total Bet Number') }}
-                    </x-nav-link>
-                </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-20 sm:flex">
-{{--                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">--}}
-                    <x-nav-link>
-                        {{ __('Win Report') }}
-                    </x-nav-link>
-                </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-20 sm:flex">
-{{--                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">--}}
-                    <x-nav-link>
-                        {{ __('Daily Report') }}
-                    </x-nav-link>
-                </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-20 sm:flex">
-{{--                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">--}}
-                    <x-nav-link>
-                        {{ __('Summary Report') }}
-                    </x-nav-link>
-                </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-20 sm:flex">
-{{--                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">--}}
-                    <x-nav-link>
-                        {{ __('Results') }}
-                    </x-nav-link>
-                </div>
+
             </div>
 
             <!-- Settings Dropdown -->
             <div class="sm:flex sm:items-center sm:ms-6">
-            <div class="hidden space-x-8 sm:-my-px sm:ms-20 sm:flex">
-{{--                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">--}}
-                    <x-nav-link>
-                        {{ __('Results') }}
-                    </x-nav-link>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-20 sm:flex">
+                    <x-nav-link style="font-size: 16px;font-weight: 800; color:white">{{ Auth::user()->name ?? 'Guest' }}</x-nav-link>
+                    <x-nav-link style="font-size: 16px;font-weight: 800; color:white">{{ __('Change Password') }}</x-nav-link>
+                    <x-nav-link style="font-size: 16px;font-weight: 800; color:white">{{ __('Change Language') }}</x-nav-link>
+                    <x-nav-link style="font-size: 16px;font-weight: 800; color:white">{{ __('Logout') }}</x-nav-link>
                 </div>
             </div>
 
