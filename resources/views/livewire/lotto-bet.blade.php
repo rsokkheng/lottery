94a3187d1af0d45e7d5542f42c56ba4e7b9f4691
uksamon <75741140+uksamon@users.noreply.github.com>
2025-03-29 23:35:27 +0700

    Create responsive page input bet

--- resources/views/livewire/lotto-bet.blade.php
+++ resources/views/livewire/lotto-bet.blade.php
@@ -1,5 +1,6 @@
 <div>
-    <div class="grid grid-cols-[30%_68%] gap-4 mx-auto bg-white shadow-md py-4 rounded-lg space-x-2">
+{{--    <div class="sm:bg-green-500 md:bg-blue-500 lg:bg-red-500 xl:bg-gray-500 h-6"> Colloer</div>--}}
+    <div class="grid xl:grid-cols-[30%_68%] gap-4 mx-auto bg-white shadow-md py-4 rounded-lg space-x-2">
         <div class="w-full px-2">
             <div class="mb-6">
                 <div class="w-full max-w-md mx-auto  rounded-lg">
@@ -56,38 +57,35 @@
                         </tr>
                         </tbody>
                     </table>
+                    <div class="flex p-2 items-center">
+                        <div class="px-2 text-[12px]">Chi Chu:</div>
+                        <div>
+                            <span class="border border-gray-500 text-[12px]  px-1 py-1">{{ __('2so x 75') }}</span>
+                            <span class="border border-gray-500 text-[12px]  px-1 py-1">{{ __('3so x 650') }}</span>
+                            <span class="border border-gray-500 text-[12px]  px-1 py-1">{{ __('4so x 6000') }}</span>
+                        </div>
+                    </div>
+                    <p class="text-[12px] pb-2 text-center font-bold">{{ __('LƯU Ý: PHIẾU CHỈ CÓ GIÁ TRỊ TRONG 3 NGÀY') }}
+                    </p>
+                    <!-- Print Button -->
+                    <button wire:click="handleSave"
+                            class="w-full flex justify-center items-center bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                             stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                  d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0 1 10.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0 .229 2.523a1.125 1.125 0 0 1-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0 0 21 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 0 0-1.913-.247M6.34 18H5.25A2.25 2.25 0 0 1 3 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 0 1 1.913-.247m10.5 0a48.536 48.536 0 0 0-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5Zm-3 0h.008v.008H15V10.5Z"/>
+                        </svg>
+                        {{ __(' PRINT') }}
+                    </button>
                 </div>
 
             </div>
             <!-- Note Section -->
-            <div class="mb-4">
-                <div class="flex pb-2 items-center">
-                    <div class="px-2 text-[12px]">Chi Chu:</div>
-                    <div>
-                        <span class="border border-gray-500 text-[12px]  px-1 py-1">{{ __('2so x 75') }}</span>
-                        <span class="border border-gray-500 text-[12px]  px-1 py-1">{{ __('3so x 650') }}</span>
-                        <span class="border border-gray-500 text-[12px]  px-1 py-1">{{ __('4so x 6000') }}</span>
-                    </div>
-                </div>
-                <p class="text-[12px] pb-2 text-center font-bold">{{ __('LƯU Ý: PHIẾU CHỈ CÓ GIÁ TRỊ TRONG 3 NGÀY') }}
-                </p>
-                <!-- Print Button -->
-                <button wire:click="handleSave"
-                        class="w-full flex justify-center items-center bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                         stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                              d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0 1 10.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0 .229 2.523a1.125 1.125 0 0 1-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0 0 21 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 0 0-1.913-.247M6.34 18H5.25A2.25 2.25 0 0 1 3 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 0 1 1.913-.247m10.5 0a48.536 48.536 0 0 0-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5Zm-3 0h.008v.008H15V10.5Z"/>
-                    </svg>
-                    {{ __(' PRINT') }}
-                </button>
-
-            </div>
 
         </div>
 
         <!-- Table header Section -->
-        <div class="overflow-auto w-full mx-auto">
+        <div class="overflow-auto w-full mx-auto relative">
             <table class="w-full text-sm border-collapse border border-gray-300">
                 <thead>
                     <span><small style="color:black;font-size:18px;font-weight: bold;">Time Left:</small>
@@ -140,7 +138,7 @@
                                     autocomplete="off"
                                     wire:model.defer="number.{{ $i }}"
                                     wire:input="handleInputNumber"
-                                    class="w-full h-8 rounded"
+                                    class="w-[100px] sm:w-full h-8 rounded"
                                     oninput="formatNumberInput(this)">
                         </td>
                         <!--Digit-->
@@ -155,7 +153,7 @@
                                         wire:model="a_amount.{{ $i }}"
                                         wire:input="handleInputAmount({{$i}})"
                                         {{ isset($enableChanelA[$i]) && $enableChanelA[$i] ? '' : 'disabled' }}
-                                        class="w-full h-8 rounded focus:ring-0 translate-0 {{ isset($enableChanelA[$i]) && $enableChanelA[$i] ? 'bg-white' : 'bg-gray-200 cursor-no-drop' }}"
+                                        class="w-[100px] sm:w-full h-8 rounded focus:ring-0 translate-0 {{ isset($enableChanelA[$i]) && $enableChanelA[$i] ? 'bg-white' : 'bg-gray-200 cursor-no-drop' }}"
                                         oninput="formatNumberValue(this)">
                                 <input type="checkbox"
                                        id="a_check_{{ $i }}"
@@ -174,7 +172,7 @@
                                         wire:input="handleInputAmount({{$i}})"
                                         wire:input="handleInputNumber"
                                         :disabled="{{ isset($enableChanelB[$i]) && $enableChanelB[$i] ? 'false' : 'true' }}"
-                                        class="w-full h-8 rounded {{ isset($enableChanelB[$i]) && $enableChanelB[$i] ? 'bg-white' : 'bg-gray-200 cursor-no-drop' }}"
+                                        class="w-[100px] sm:w-full h-8 rounded {{ isset($enableChanelB[$i]) && $enableChanelB[$i] ? 'bg-white' : 'bg-gray-200 cursor-no-drop' }}"
                                         oninput="formatNumberValue(this)">
 
                                 <input
@@ -193,7 +191,7 @@
                                         wire:model="ab_amount.{{ $i }}"
                                         wire:input="handleInputAmount({{$i}})"
                                         {{ isset($enableChanelAB[$i]) && $enableChanelAB[$i] ? '' : 'disabled' }}
-                                        class="w-full h-8 rounded {{ isset($enableChanelAB[$i]) && $enableChanelAB[$i] ? 'bg-white' : 'bg-gray-200 cursor-no-drop' }}"
+                                        class="w-[100px] sm:w-full h-8 rounded {{ isset($enableChanelAB[$i]) && $enableChanelAB[$i] ? 'bg-white' : 'bg-gray-200 cursor-no-drop' }}"
                                         oninput="formatNumberValue(this)">
                                 <input
                                         type="checkbox"
@@ -212,7 +210,7 @@
                                         wire:model="roll_amount.{{ $i }}"
                                         wire:input="handleInputAmount({{$i}})"
                                         {{ isset($enableChanelRoll[$i]) && $enableChanelRoll[$i] ? '' : 'disabled' }}
-                                        class="w-full h-8 rounded {{ isset($enableChanelRoll[$i]) && $enableChanelRoll[$i] ? 'bg-white' : 'bg-gray-200 cursor-no-drop' }}"
+                                        class="w-[100px] sm:w-full h-8 rounded {{ isset($enableChanelRoll[$i]) && $enableChanelRoll[$i] ? 'bg-white' : 'bg-gray-200 cursor-no-drop' }}"
                                         oninput="formatNumberValue(this)">
                                 <input
                                         type="checkbox"
@@ -230,7 +228,7 @@
                                         wire:model="roll7_amount.{{ $i }}"
                                         wire:input="handleInputAmount({{$i}})"
                                         {{ isset($enableChanelRoll7[$i]) && $enableChanelRoll7[$i] ? '' : 'disabled' }}
-                                        class="w-full h-8 rounded {{ isset($enableChanelRoll7[$i]) && $enableChanelRoll7[$i] ? 'bg-white' : 'bg-gray-200 cursor-no-drop' }}"
+                                        class="w-[100px] sm:w-full h-8 rounded {{ isset($enableChanelRoll7[$i]) && $enableChanelRoll7[$i] ? 'bg-white' : 'bg-gray-200 cursor-no-drop' }}"
                                         oninput="formatNumberValue(this)">
                                 <input
                                         type="checkbox"
@@ -248,7 +246,7 @@
                                         wire:model="roll_parlay_amount.{{ $i }}"
                                         wire:input="handleInputAmount({{$i}})"
                                         {{ isset($enableChanelRollParlay[$i]) && $enableChanelRollParlay[$i] ? '' : 'disabled' }}
-                                        class="w-full h-8 rounded {{ isset($enableChanelRollParlay[$i]) && $enableChanelRollParlay[$i] ? 'bg-white' : 'bg-gray-200 cursor-no-drop' }}"
+                                        class="w-[100px] sm:w-full h-8 rounded {{ isset($enableChanelRollParlay[$i]) && $enableChanelRollParlay[$i] ? 'bg-white' : 'bg-gray-200 cursor-no-drop' }}"
                                         oninput="formatNumberValue(this)">
                                 <input
                                         type="checkbox"
