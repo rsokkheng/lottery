3948fa1fef69696365ab0fb9f30163686ab6ea08
uksamon <75741140+uksamon@users.noreply.github.com>
2025-03-30 22:31:15 +0700

    Update lotto-bet.blade.php

--- resources/views/livewire/lotto-bet.blade.php
+++ resources/views/livewire/lotto-bet.blade.php
@@ -66,7 +66,7 @@
                     </p>
                     <!-- Print Button -->
                     <button wire:click="handleSave"
-                            class="hidden w-full xl:flex justify-center items-center bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                            class="hidden w-full lg:flex justify-center items-center bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                              stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round"
@@ -76,7 +76,7 @@
                     </button>
 
                     <button wire:click="handleSave"
-                            class="xl:hidden w-full flex justify-center items-center bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                            class="lg:hidden w-full flex justify-center items-center bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 9.75v6.75m0 0-3-3m3 3 3-3m-8.25 6a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
                         </svg>
