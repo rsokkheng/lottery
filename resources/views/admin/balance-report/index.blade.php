1333917b9eb652dbecf6e7023837da55c429c4a1
rsokkheng <sokkhengry168@gmail.com>
2025-04-29 07:32:39 +0700

    remove file

--- resources/views/admin/balance-report/index.blade.php
+++ resources/views/admin/balance-report/index.blade.php
@@ -1,65 +1,50 @@
 <x-admin>
-    @section('title', 'Users')
+    @section('title', 'Account Balance')
     <div class="card">
-        <div class="card-header">
-            <h3 class="card-title">User Table</h3>
-            <div class="card-tools"><a href="{{ route('admin.user.create') }}" class="btn btn-sm btn-primary">Add New</a></div>
-        </div>
+       
         <div class="card-body">
             <table class="table table-striped" id="userTable">
                 <thead>
                     <tr style="font-size: 14px;">
                         <th>#</th>
-                        <th>Role Name</th>
-                        <th>Account ID</th>
                         <th>Name</th>
-                        <th>Package</th>
-                        <th>Available Credit</th>
-                        <th>Bet Credit</th>
-                        <th>Cash Balance</th>
-                        <th>Register Date</th>
-                        <th>Last Login</th>
+                        <th>Account ID</th>
+                        <th>Beginning</th>
+                        <th>Net W/L</th>
+                        <th>Deposit</th>
+                        <th>Withdraw</th>
+                        <th>Adjustment</th>
+                        <th>Balance</th>
+                        <th>Outstanding</th>
+                        <th>Status</th>
                         <th>Action</th>
-                  
                     </tr>
                 </thead>
                 <tbody>
                    
                     @foreach ($data as $key => $user)
+                    @foreach ($user->userWallet as $wallet)
                         <tr>
                             <td>{{ $key+1 }}</td>
-                            <td> @foreach ($user->roles as $role)
-                                    <span class="badge bg-success">{{ $role->name }}</span>
-                                @endforeach
-                            </td>
-                            <td>{{ $user->username }}</td>
                             <td>{{ $user->name }}</td>
-                            <td>{{ $user->package->package_code }}</td>
-                            <td> @foreach ($user->userWallet as $wallet)
-                                    <span>{{ $wallet->available_credit}}</span>
-                                @endforeach
-                            </td>
-                            <td>@foreach ($user->userWallet as $wallet)
-                                    <span>{{ $wallet->given_credit }}</span>
-                                @endforeach
-                            </td>
+                            <td>{{ $user->username }}</td>
+                            <td>{{ $wallet->beginning }}</td>
+                            <td>{{ $wallet->net_win_loss }}</td>
+                            <td>{{ $wallet->deposit }}</td>
+                            <td>{{ $wallet->withdraw }}</td>
+                            <td>{{ $wallet->balance }}</td>
+                            <td>{{ $wallet->adjustment }}</td>
+                            <td>{{ $wallet->outstanding }}</td>
+                            <td>{{ $wallet->status }}</td>
+                            
                             <td>
-                            @foreach ($user->userWallet as $wallet)
-                                    <span>{{ $wallet->balance }}</span>
-                                @endforeach
-                            </td>
-                            <td>{{ $user->created_at }}</td>
-                            <td>{{ $user->updated_at }}</td>
-                            <td>
-                                <a href="{{ route('admin.user.edit', encrypt($user->id)) }}" class="btn btn-sm btn-primary" style="display: inline-block; margin-right: 5px;">Edit</a> 
-                                <form action="{{ route('admin.user.destroy', encrypt($user->id)) }}" method="POST" onsubmit="return confirm('Are sure want to delete?')" style="display: inline-block;">
-                                    @method('DELETE')
-                                    @csrf
-                                    <button type="submit" class="btn btn-sm btn-danger">Delete</button>
-                                </form>
+                                <a href="{{ route('admin.user.edit', encrypt($user->id)) }}" class="btn btn-sm btn-primary" style="display: inline-block; margin-right: 5px;">Deposit</a> 
+                                <a href="{{ route('admin.user.edit', encrypt($user->id)) }}" class="btn btn-sm btn-danger" style="display: inline-block; margin-right: 5px;">Withdraw</a> 
+                              
                             </td>
 
                         </tr>
+                        @endforeach
                     @endforeach
                 </tbody>
             </table>
